a1e84b0d2f6b749a64543dbc84e92e17d94156fb
Daniel <julykitt@gmail.com>
2015-09-13 21:40:05 +0800

    update shadowsocks install script

--- installShadowsocks.sh
+++ installShadowsocks.sh
@@ -2,5 +2,5 @@
 echo "src/gz openwrt_dist http://openwrt-dist.sourceforge.net/releases/ar71xx/packages" >> /etc/opkg.conf
 echo "src/gz openwrt_dist_luci http://openwrt-dist.sourceforge.net/releases/luci/packages" >> /etc/opkg.conf
 opkg install ChinaDNS luci-app-chinadns shadowsocks-libev-spec  luci-app-shadowsocks-spec luci-i18n-chinese
-wget -O- 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | awk -F\| '/CN\|ipv4/ { printf(%s/%dn, , 32-log()/log(2)) }' > /etc/ignore.list 
+wget -O- 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > /etc/ignore.list
 echo "ignore list is store in /etc/ignore.list"
